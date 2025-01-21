61176ed0e2f23bf52103c52436ed8161204d80d9
Sofia-Arias017 <189870116+Sofia-Arias017@users.noreply.github.com>
2025-01-21 00:30:44 -0500

    se puede buscar autor

--- funcional/buscarElemento.py
+++ funcional/buscarElemento.py
@@ -24,6 +24,27 @@ def buscarTitulo():
     print('No se ha encontrado.')
     pressEnter()
 
+
+def buscarAutor():
+    titulo = input('Escribe el Autor/Compositor/Director a buscar :').lower()
+    for colec in coleccion['libros']:
+        if titulo == colec["autor"]:
+            print(tabulate([colec], headers="keys", tablefmt="grid"))
+            pressEnter()
+            return
+    for colec in coleccion['musica']:
+        if titulo == colec["autor"]:
+            print(tabulate([colec], headers="keys", tablefmt="grid"))
+            pressEnter()
+            return
+    for colec in coleccion['peliculas']:
+        if titulo == colec["autor"]:
+            print(tabulate([colec], headers="keys", tablefmt="grid"))
+            pressEnter()
+            return
+    print('No se ha encontrado.')
+    pressEnter()
+
 def buscarElemento():
     while True:
         print(buscarElementoDiseño)
@@ -32,8 +53,7 @@ def buscarElemento():
             case 1:
                 buscarTitulo()
             case 2:
-                 #buscarTitulo()
-                pass
+                 buscarAutor()
             case 3:
                  #buscarTitulo()
                 pass
